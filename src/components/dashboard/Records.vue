7cad914e847491e08ca9a0d4e0be01669a81136e
Sagar <sagar@adolpha.com>
2018-06-06 19:02:51 +0530

    dynamic record add v1

--- src/components/dashboard/Records.vue
+++ src/components/dashboard/Records.vue
@@ -1,11 +1,33 @@
 <template>
- <div>
-     Records
- </div>
+
+
+<div id="container">
+<button @click="addRow">Add</button>
+  <div  v-for="row in rows" :key="row.id">
+    <div class="input-field col s12 m12 l12">
+      <input type="text" :value="row.full_name">
+      <input type="text" :value="row.last_name">
+    </div>
+  </div>
+
+</div>
+
 </template>
 
 <script>
   export default {
-    
+    data() {
+      return {
+        rows : [
+          {id:1, full_name: "john", last_name: "doe"}, 
+          {id:2, full_name: "jane", last_name: "so"}
+        ]
+      }
+    },
+    methods: {
+      addRow() {
+        this.rows.push({full_name: "damn", last_name: "damn so"})
+      }
+    }
   }
 </script>
